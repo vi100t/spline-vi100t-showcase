eabe230c15a7e3b8000954f283e08df173558d3a
Vicente <dovallevicente@gmail.com>
2025-06-09 20:12:27 -0300

    netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import netlify from "@netlify/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,8 +12,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
+    netlify(),
   ].filter(Boolean),
   resolve: {
     alias: {
